3509e8bbbf653c3218c07ba6d81268c0040976de
sathvikgadipally <89178620+sathvikgadipally@users.noreply.github.com>
2022-06-26 13:51:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pywhatkit as py
 
 
-phno = input('What is the phone number?(with +91): ')
+phno = input('What is the phone number?(Make sure that you enter your country''s dial code'): ')
 messg = input('What is the message?(leave a space at the end): ')
-times = input('How many time should the message be spammed?: ')
-timHrs = input('What is the time in Hours?: ')
-timMin = input('What is the time in minutes?: ')
+times = input('How many time should the message be sent?: ')
+timHrs = input('What is the time(hours)?: ')
+timMin = input('What is the time(minutes)?: ')
 message = py.sendwhatmsg(phno, messg  * int(times), timHrs, timMin)
